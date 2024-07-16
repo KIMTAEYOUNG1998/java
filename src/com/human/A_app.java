ab781f644134e5def7560636ef26cce6c5ebe4c1
김태영 <k01707@naver.com>
2024-07-16 16:28:28 +0900

    커밋

---
+++ src/com/human/A_app.java
@@ -0,0 +1,19 @@
+package com.human;
+
+import com.human.a_variable.VariableType;
+
+public class A_app {
+    public static void main(String[] args) {
+        // 생성
+        VariableType v = new VariableType();
+        // 실행
+        v.type1();
+        System.out.println("=============================");
+        v.type3();
+        v.type5();
+        v.type6();
+        v.type4();
+        v.type2();
+        
+    }
+}
---
+++ src/com/human/A_variableApp.java
@@ -0,0 +1,17 @@
+package com.human;
+
+import com.human.a_variable.Variable;
+
+public class A_variableApp {
+    public static void main(String[] args) {
+        // 생성
+        Variable v = new Variable();
+        // 호출(변수명. 메서드명())
+        v.var();
+        v.var2();
+        System.out.println();
+        // 메서드 호출
+        v.gugudan();
+        v.var3(); 
+    }
+}
